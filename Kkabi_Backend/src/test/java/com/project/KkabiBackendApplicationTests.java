fd94bf95f652946c3049a7d66172f1a29f57f54f
seojisoosoo <amy0480@naver.com>
2023-05-15 17:11:36 +0900

    fix: 잘못된 객체 생성으로 인한 오류 수정

--- Kkabi_Backend/src/test/java/com/project/KkabiBackendApplicationTests.java
+++ Kkabi_Backend/src/test/java/com/project/KkabiBackendApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Commit;
 
-import com.project.domain.AccountList;
+import com.project.domain.AccountInfo;
 import com.project.repository.AccountInfoRepository;
 
 @SpringBootTest
@@ -24,10 +24,10 @@ class KkabiBackendApplicationTests {
 
 	@Test
 	void accountInfoInsert() {
-		accountInfoRep.save(new AccountList(1, "깨비 미래 적금", 10.0, "12일 만기, 이자율 10%"));
-		accountInfoRep.save(new AccountList(2, "깨비 young 적금", 5.0, "6일 만기, 이자율 5%"));
-		accountInfoRep.save(new AccountList(3, "깨비 자유로운 예금", 0.0, "매일 100원 지급, 예금 한도 하루 5만원"));
-		accountInfoRep.save(new AccountList(4, "깨비 super young 예금", 0.0, "매일 500원 지급, 예금 한도 하루 1만원"));
+		accountInfoRep.save(new AccountInfo(1, "깨비 미래 적금", 10.0, "12일 만기, 이자율 10%"));
+		accountInfoRep.save(new AccountInfo(2, "깨비 young 적금", 5.0, "6일 만기, 이자율 5%"));
+		accountInfoRep.save(new AccountInfo(3, "깨비 자유로운 예금", 0.0, "매일 100원 지급, 예금 한도 하루 5만원"));
+		accountInfoRep.save(new AccountInfo(4, "깨비 super young 예금", 0.0, "매일 500원 지급, 예금 한도 하루 1만원"));
 		
 	}
 }
