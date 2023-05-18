b20eadb01bcd049e5c3eb92789009c909c8025ea
Awarduuu <award_uuu@naver.com>
2023-05-18 16:39:50 +0900

    fix: selectMyAccountList jpql 수정

--- Kkabi_Backend/src/main/java/com/project/repository/AccountListRepository.java
+++ Kkabi_Backend/src/main/java/com/project/repository/AccountListRepository.java
@@ -3,6 +3,7 @@ package com.project.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
@@ -14,5 +15,11 @@ public interface AccountListRepository extends JpaRepository<AccountList, Intege
 	
 	@Query(value="select a from AccountList a where a.user.userSeq = :userSeq")
 	List<AccountList> findAccountListByUserSeq(@Param("userSeq") int userseq);
+	
+	@Query(value="update AccountList a "
+			+ "set a.accountMoney = :accountLogMoney "
+			+ "where a.accountId = :accountId")
+	@Modifying
+	void updateAccountMoney(@Param("accountLogMoney") int accountLogMoney, @Param("accountId") int accountId);
 	
 }
