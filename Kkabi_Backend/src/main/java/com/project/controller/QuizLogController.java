94a9377ffd4c00c6f17a72e94419336532fa5b7e
seojisoosoo <amy0480@naver.com>
2023-05-19 14:09:39 +0900

    feat: 컨트롤러작성

--- Kkabi_Backend/src/main/java/com/project/controller/QuizLogController.java
+++ Kkabi_Backend/src/main/java/com/project/controller/QuizLogController.java
@@ -1,9 +1,12 @@
 package com.project.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.project.domain.QuizLog;
 import com.project.service.QuizLogService;
 
 @RestController
@@ -15,8 +18,13 @@ public class QuizLogController {
 	
 	
 	/**
-	 *  전체 검색 페이지
+	 *  퀴즈 내역 조회 -> 전체 조회
+	 *  @author seojisoosoo
 	 */
 	@GetMapping("/quiz-log")
-	public void quizLog();
+	public List<QuizLog> selectQuizLog(int quizId){
+		List<QuizLog> quizlogList=quizLogservice.selectQuizLog(quizId);
+		
+		return quizlogList;
+	}
 }
