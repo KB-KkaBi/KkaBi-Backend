25d441e9d2d58d8b8fddeeab2ebb071fdb0171cd
Awarduuu <award_uuu@naver.com>
2023-05-21 01:03:08 +0900

    refact: AccountLogRepository의findAllByAccountListJPQL return 자료형 Page로 변환 및 JPQL 내림차순 추가

--- Kkabi_Backend/src/main/java/com/project/repository/AccountLogRepository.java
+++ Kkabi_Backend/src/main/java/com/project/repository/AccountLogRepository.java
@@ -1,7 +1,9 @@
 package com.project.repository;
 
-import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -9,7 +11,10 @@ import org.springframework.data.repository.query.Param;
 import com.project.domain.AccountLog;
 
 public interface AccountLogRepository extends JpaRepository<AccountLog, Integer>{
+	int nowPage = 1;
+	int PAGE_COUNT = 10;
+	Pageable page = PageRequest.of((nowPage-1), PAGE_COUNT);
 	
-	@Query(value="select a from AccountLog a where a.accountList.accountId = :accountId")
-	List<AccountLog> findAllByAccountListJPQL(@Param("accountId") int accountId);
+	@Query(value="select a from AccountLog a where a.accountList.accountId = :accountId order by a.accountLogId desc")
+	Page<AccountLog> findAllByAccountListJPQL(@Param("accountId") int accountId, Pageable page);
 }
