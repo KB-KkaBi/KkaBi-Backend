2b8c7a642debc65a34442198f3b96f6979c4a5cf
Awarduuu <award_uuu@naver.com>
2023-05-20 17:43:15 +0900

    etc: InsertAccountLogForInterest Api Operation 작성

--- Kkabi_Backend/src/main/java/com/project/controller/AccountLogController.java
+++ Kkabi_Backend/src/main/java/com/project/controller/AccountLogController.java
@@ -56,6 +56,10 @@ public class AccountLogController {
 		return accountLogList;
 	}
 	
+	/**
+	 * @author awarduuu
+	 */
+	@ApiOperation(value="모든 계좌 이자 지급" ,notes="매일 0시 0분 1초에 모든 계좌를 확인하여 이자를 지급할 때 사용된다.")
 	@Scheduled(cron = "1 0 0 * * *")
 	public void insertAccountLogForInterest() {
 		accountLogService.insertAccountLogForInterest();
