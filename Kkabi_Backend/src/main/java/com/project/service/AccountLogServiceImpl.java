8cbb7bb2aebd9a0de0556797d3b083e3556bed26
Awarduuu <award_uuu@naver.com>
2023-05-20 16:10:03 +0900

    feat: AccountLogServiceImpl 이자 로그 기록 메소드 완성 (리팩토링 필요)

--- Kkabi_Backend/src/main/java/com/project/service/AccountLogServiceImpl.java
+++ Kkabi_Backend/src/main/java/com/project/service/AccountLogServiceImpl.java
@@ -1,12 +1,16 @@
 package com.project.service;
 
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.project.domain.AccountList;
 import com.project.domain.AccountLog;
 import com.project.repository.AccountListRepository;
 import com.project.repository.AccountLogRepository;
@@ -38,6 +42,64 @@ public class AccountLogServiceImpl implements AccountLogService{
 		List<AccountLog> accountLogList = accountLogRep.findAllByAccountListJPQL(accountId);
 		
 		return accountLogList;
+	}
+
+	
+	@Override
+	@Scheduled(cron = "0 0 0 * * *")
+	public void insertAccountLogForInterest(int userSeq) {
+		
+		List<AccountList> accountList = accountListRep.findAccountListByUserSeq(userSeq);
+		
+		// 1. 가져온 내 계좌가 있다면
+		if(accountList != null) {
+			for(AccountList l : accountList) {
+				
+				int accountInfoId = l.getAccountInfo().getAccountInfoId();
+				String status = l.getStatus();
+				LocalDate current = LocalDate.now();
+				
+				// 2. 내 계좌가 활성화 되어있다면
+				if(status.equals("1")) {
+					// 3-1. 계좌가 깨비 미래 적금이라면
+					if(accountInfoId == 1) {
+						// 4. 현재 날짜가 적금만기일 다음이라면
+						if(current.isAfter(l.getCreatedAt().toLocalDate().plusDays(12))) {
+							// 내 계좌 업데이트
+							int interest =(int)(l.getAccountMoney() * (l.getAccountInfo().getInterestRate()));
+							l.setAccountMoney(l.getAccountMoney() + interest);
+							l.setStatus("2");
+							
+							// 계좌 로그 기록
+							accountLogRep.save(AccountLog.builder()
+									.accountList(l)
+									.accountLogMoney(l.getAccountMoney() + interest)
+									.transactionAmount(interest)
+									.transactionReason("만기 이자")
+									.transactionType("이자")
+									.build());
+						}
+					}
+					// 3-2. 계좌가 깨비 young 적금이라면
+					else if(accountInfoId == 2) {
+						if(current.isAfter(l.getCreatedAt().toLocalDate().plusDays(6))) {
+							int interest =(int)(l.getAccountMoney() * (l.getAccountInfo().getInterestRate()));
+							l.setAccountMoney(l.getAccountMoney() + interest);
+							l.setStatus("2");
+							
+							accountLogRep.save(AccountLog.builder()
+									.accountList(l)
+									.accountLogMoney(l.getAccountMoney() + interest)
+									.transactionAmount(interest)
+									.transactionReason("만기 이자")
+									.transactionType("이자")
+									.build());
+						}
+					}
+				}
+				
+			}
+		}
 	}
 	
 	
