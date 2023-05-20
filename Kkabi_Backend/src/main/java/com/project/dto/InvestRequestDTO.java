ee1a0ce13eb5e07e473d9586ac1dd5047e8a9c45
Awarduuu <award_uuu@naver.com>
2023-05-21 00:12:41 +0900

    style: InvestRequestDTO ApiModelDescription 작성

--- Kkabi_Backend/src/main/java/com/project/dto/InvestRequestDTO.java
+++ Kkabi_Backend/src/main/java/com/project/dto/InvestRequestDTO.java
@@ -2,6 +2,8 @@ package com.project.dto;
 
 import java.time.LocalDateTime;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -13,10 +15,30 @@ import lombok.Setter;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
+@ApiModel(description = "InvesController의 submit에 필요한 값을 받기 위한 DTO")
 public class InvestRequestDTO {
 	
+	@ApiModelProperty(
+			value = "퀴즈의 고유 id",
+			example = "1",
+			required = true)
 	private int quizId;
+	
+	@ApiModelProperty(
+			value = "보물의 고유 id",
+			example = "2",
+			required = true)
 	private int treasureId;
+	
+	@ApiModelProperty(
+			value = "투자한 보물의 개수",
+			example = "100",
+			required = true)
 	private int count;
+	
+	@ApiModelProperty(
+			value = "만든 정답",
+			example = "주식",
+			required = true)
 	private String answer;
 }
