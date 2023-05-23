e7f1eb10f153df361d612dbe1350ad1783f35644
Awarduuu <award_uuu@naver.com>
2023-05-23 16:13:24 +0900

    feat: TreasureLogService에 로직 작성

--- Kkabi_Backend/src/main/java/com/project/service/TreasureLogServiceImpl.java
+++ Kkabi_Backend/src/main/java/com/project/service/TreasureLogServiceImpl.java
@@ -1,10 +1,12 @@
 package com.project.service;
 
+import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 
 import com.project.dto.DetailTreasureDTO;
+import com.project.dto.Treasure;
 import com.project.repository.TreasureLogRepository;
 
 public class TreasureLogServiceImpl implements TreasureLogService {
@@ -17,9 +19,28 @@ public class TreasureLogServiceImpl implements TreasureLogService {
 		
 		DetailTreasureDTO detailTreasure = new DetailTreasureDTO();
 		
-		Map<Integer, Integer> map = treasureLogRep.selectDetailTreasureJPQL(userSeq);
-				
-		return null;
+		List<Treasure> t = treasureLogRep.selectDetailTreasureJPQL(userSeq);
+		
+		for(Treasure item : t) {
+			switch (item.getTreasure()) {
+			case 1:
+				detailTreasure.setTreasure1(item.getCnt());
+				break;
+			case 2:
+				detailTreasure.setTreasure2(item.getCnt());
+				break;
+			case 3:
+				detailTreasure.setTreasure3(item.getCnt());
+				break;
+			case 4:
+				detailTreasure.setTreasure4(item.getCnt());
+				break;
+			default:
+				break;
+			}
+		}
+		
+		return detailTreasure;
 	}
 
 }
