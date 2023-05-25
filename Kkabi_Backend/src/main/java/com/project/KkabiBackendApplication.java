19eb7b11f245b628928e08e6ec422c6f6a2fc098
SangWoo Yun <64531982+Awarduuu@users.noreply.github.com>
2023-05-25 09:15:00 +0900

    Fix#53-user/login (#54)
    * fix: 클라이언트로 쿠키 보내기
    
    * fix: same-site 설정 취소

--- Kkabi_Backend/src/main/java/com/project/KkabiBackendApplication.java
+++ Kkabi_Backend/src/main/java/com/project/KkabiBackendApplication.java
@@ -24,7 +24,10 @@ public class KkabiBackendApplication {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**").allowedOrigins("https://kb-kkabi.web.app", "http://localhost:5173", "http://localhost:5174");
+                registry.addMapping("/**").allowedOrigins("https://kb-kkabi.web.app", "http://localhost:5173", "http://localhost:5174")
+                .allowedHeaders("*")
+                .allowedMethods("*")
+                .allowCredentials(true);
             }
         };
     }
